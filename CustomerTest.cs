408c18bec724ca27d2dd1c956d2a929ec9ce2ca2
Vinh Timoon <vinhm281@gmail.com>
2025-06-02 11:18:13 +0700

    Update

--- CustomerTest.cs
+++ CustomerTest.cs
@@ -6,18 +6,21 @@ namespace Lab1_V2.Tests
     [TestFixture]
     public class CustomerTest
     {
+        private Customer customer;
+
+
         [SetUp]
         public void Setup()
         {
             // Code này sẽ chạy trước mỗi test case
-            Customer customer = new Customer();
+            customer = new Customer();
         }
 
         [Test]
         public void CheckAge_AgeIs18_ReturnsTrue()
         {
             // Arrange
-            Customer customer = new Customer { Age = 18 };
+            customer.Age = 18;
 
             // Act
             bool result = customer.CheckAge();
@@ -30,7 +33,7 @@ namespace Lab1_V2.Tests
         public void CheckAge_AgeIs60_ReturnsTrue()
         {
             // Arrange
-            Customer customer = new Customer { Age = 60 };
+            customer.Age = 60;
 
             // Act
             bool result = customer.CheckAge();
@@ -43,7 +46,7 @@ namespace Lab1_V2.Tests
         public void CheckAge_AgeIs419_ReturnsTrue()
         {
             // Arrange
-            Customer customer = new Customer { Age = 19 };
+            customer.Age = 19;
 
             // Act
             bool result = customer.CheckAge();
@@ -56,20 +59,33 @@ namespace Lab1_V2.Tests
         public void CheckAge_AgeIs17_ReturnsFalse()
         {
             // Arrange
-            Customer customer = new Customer { Age = 17 };
+            customer.Age = 17;
 
             // Act
             bool result = customer.CheckAge();
 
             // Assert
             Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void CheckAge_AgeIs59_ReturnsFalse()
+        {
+            // Arrange
+            customer.Age = 59;
+
+            // Act
+            bool result = customer.CheckAge();
+
+            // Assert
+            Assert.IsTrue(result);
         }
 
         [Test]
         public void CheckAge_AgeIs61_ReturnsFalse()
         {
             // Arrange
-            Customer customer = new Customer { Age = 61 };
+            customer.Age = 61;
 
             // Act
             bool result = customer.CheckAge();
@@ -78,23 +94,22 @@ namespace Lab1_V2.Tests
             Assert.IsFalse(result);
         }
 
-        [Test]
-        public void SetAge_NegativeAge_ThrowsArgumentException()
-        {
-            // Arrange
-            Customer customer = new Customer();
+        // [Test]
+        // public void SetAge_NegativeAge_ThrowsArgumentException()
+        // {
+        //     // Arrange
 
-            // Act & Assert
-            Assert.Throws<ArgumentException>(() =>
-            {
-                customer.Age = -5;
-            });
-        }
+        //     // Act & Assert
+        //     Assert.Throws<ArgumentException>(() =>
+        //     {
+        //         customer.Age = -5;
+        //     });
+        // }
 
         [TearDown]
         public void Cleanup()
         {
-
+            customer = null;
         }
     }
 }
--- obj/Debug/net6.0/Lab1_V2.AssemblyInfo.cs
+++ obj/Debug/net6.0/Lab1_V2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1_V2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f8683da201e35b2605581caf34c6b5c47f97714")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1_V2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1_V2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
